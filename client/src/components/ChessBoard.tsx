8434e6bab45c7a9da508c0c6ee5b637da28c5882
Nicholas Carnival <carnivalnicholas2@gmail.com>
2024-07-23 22:57:41 -0600

    promotion problems

--- client/src/components/ChessBoard.tsx
+++ client/src/components/ChessBoard.tsx
@@ -8,55 +8,57 @@ import {
 } from "react-chessboard/dist/chessboard/types";
 
 export default function PlayRandomMoveEngine() {
-  const [game, setGame] = useState(new Chess());
+  const [game, setGame] = useState(
+    new Chess("8/P1Q5/4k3/5R2/1p1b4/4P3/2P1bP2/2B1KB1R w K - 1 36")
+  );
+
+  const [promotion, setPromotion] = useState<string>("q");
+
   const makeAMove = useCallback(
-    (
-      move:
-        | string
-        | {
-            from: string;
-            to: string;
-            promotion?: string | undefined;
-          }
-    ) => {
+    (move: { from: string; to: string }) => {
       // If the move is valid, update the game state with a new Chess instance
+      console.log("making move");
       if (move !== null) {
-        game.move(move);
+        game.move({ ...move, promotion }); // Make the move
         const updatedGame = new Chess(game.fen()); // Create a new Chess instance with the updated FEN
         setGame(updatedGame); // Update the state with the new instance
       }
     },
-    [game]
+    [game, promotion]
   );
 
   const makeRandomMove = useCallback(() => {
+    console.log("making random move");
     const possibleMoves = game.moves();
     if (game.isGameOver() || game.isDraw() || possibleMoves.length === 0)
-      return; // exit if the game is over
+      return;
     const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-    makeAMove(possibleMoves[randomIndex]);
+
+    game.move(possibleMoves[randomIndex]);
+    const updatedGame = new Chess(game.fen());
+    setGame(updatedGame);
   }, [game]);
 
   const onDrop = useCallback(
     (sourceSquare: Square, targetSquare: Square, piece: Piece) => {
-      console.log("we have been dropped", targetSquare);
-      if (
-        (targetSquare.includes("8") || targetSquare.includes("1")) &&
-        piece.includes("P")
-      ) {
-        console.log("PROMOTIONS!");
-        return false;
+      console.log("dropping");
+      console.log("source", sourceSquare);
+      console.log("target", targetSquare);
+      if (targetSquare.includes("8") || targetSquare.includes("1")) {
+        console.log("PROMOTIONS!", piece);
+        // return false;
       }
 
       const move = makeAMove({
         from: sourceSquare,
         to: targetSquare,
+        // this is the problem :)
       });
 
       // illegal move
       if (move === null) return false;
 
-      setTimeout(makeRandomMove, 500);
+      setTimeout(makeRandomMove, 200);
 
       return true;
     },
@@ -69,28 +71,41 @@ export default function PlayRandomMoveEngine() {
       promoteFromSquare?: Square,
       promoteToSquare?: Square
     ) => {
+      console.log("promoting");
       if (!piece || !promoteFromSquare || !promoteToSquare) return false;
 
       const prom = convertToChessJs(piece);
+
+      setPromotion(prom);
 
       // it seems like making the promotion switches the turn...
       makeAMove({
         from: promoteFromSquare,
         to: promoteToSquare,
-        promotion: prom,
+        // promotion: prom,
       });
       return true;
     },
-    []
+    [promotion, setPromotion]
   );
 
   return (
-    <Chessboard
-      id="chessboard"
-      position={game.fen()}
-      onPieceDrop={onDrop}
-      onPromotionPieceSelect={onPromotion}
-    />
+    <>
+      <button
+        onClick={() => {
+          console.log(game.fen());
+        }}
+        className="border-2 rounded-lg text-center p-2"
+      >
+        Dump
+      </button>
+      <Chessboard
+        id="chessboard"
+        position={game.fen()}
+        onPieceDrop={onDrop}
+        onPromotionPieceSelect={onPromotion}
+      />
+    </>
   );
 }
 
